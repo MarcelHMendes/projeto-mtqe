e87ab6b716de9777b1617dea121d0f6ec03c9bef
MarcelHMendes <marcelmendes@dcc.ufmg.br>
2022-07-04 16:26:12 -0300

    add experimental files

--- analysis.py
+++ analysis.py
@@ -17,20 +17,39 @@ def run(ops, num_clients, scheduler):
         time +=  180
     return time
 
-def main():
+# 2kr experiment
+def analysis_one():
 
+    #parametro
     num_clients = 10
     num_experiments = 5
     num_rounds = 1
-    num_prefixes = 3
 
-    dataset = generate_data.clients(num_clients, num_experiments, num_rounds, num_prefixes)
+    #fator
+    num_prefixes = 4
+    total_pfx_available = 12
+
+    dataset = generate_data.clients(num_clients, num_experiments, num_rounds, num_prefixes, total_pfx_available)
     ops_heuristic = Operations(dataset, num_clients)
 
-    fd = open('heuristic.csv', 'a')
+    fd = open('heuristic_AB.csv', 'a')
     final_time = run(ops_heuristic, num_clients, maximum_throughput)
     print(final_time*num_rounds, file=fd)
     fd.close()
 
+
+def main():
+    analysis_one()
+
 if __name__ == '__main__':
     sys.exit(main())
+
+
+'''
+A (prx)  B (avail_pfx)
+2        8
+
+4        12
+
+'''
+
